02ba070034a10eb8f220160ef4f349bb5ca5698b
G- <81604568+geedash1@users.noreply.github.com>
2025-09-27 13:42:28 -0400

    Update gui.h

--- gui.h
+++ gui.h
@@ -4,33 +4,32 @@
 
 namespace gui
 {
-    // Window size constants
+    
     constexpr int WIDTH = 1920;
     constexpr int HEIGHT = 1080;
 
-    // Window state
+    
     inline bool isRunning = true;
 
-    // WinAPI window
     inline HWND window = nullptr;
     inline WNDCLASSEX windowClass = { };
     inline POINTS position = { };
 
-    // DirectX state
+
     inline PDIRECT3D9 d3d = nullptr;
     inline LPDIRECT3DDEVICE9 device = nullptr;
     inline D3DPRESENT_PARAMETERS presentParameters = { };
 
-    // Functions for window
+   
     void CreateHWindow(const char* windowName) noexcept;
     void DestroyHWindow() noexcept;
 
-    // Functions for DirectX
+   
     bool CreateDevice() noexcept;
     void ResetDevice() noexcept;
     void DestroyDevice() noexcept;
 
-    // Functions for ImGui
+    
     void CreateImGui() noexcept;
     void DestroyImGui() noexcept;
     void SetupImGuiStyle() noexcept;
@@ -39,4 +38,5 @@ namespace gui
     void EndRender() noexcept;
     void Render() noexcept;
 }
+
 
