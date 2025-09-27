5d4e1b4fa0abeaa1e968083db7a058a2bb767c29
G- <81604568+geedash1@users.noreply.github.com>
2025-09-27 13:45:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
-    // --- 1) Show loader/login ---
-    if (!ShowLoginWindow()) // loader handles its own window and device
+ 
+    if (!ShowLoginWindow()) 
     {
         MessageBoxA(nullptr, "Login failed or cancelled.", "Academia++", MB_OK | MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
-    // --- 2) Start main GUI after loader closes ---
+    \
     gui::isRunning = true;
     gui::window = nullptr;
     gui::d3d = nullptr;
@@ -28,7 +28,7 @@ int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 
     gui::CreateImGui();
 
-    // --- 3) Main GUI loop ---
+    
     while (gui::isRunning)
     {
         gui::BeginRender();
@@ -37,11 +37,12 @@ int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 
-    // --- 4) Cleanup ---
+    
     gui::DestroyImGui();
     gui::DestroyDevice();
     gui::DestroyHWindow();
 
     return EXIT_SUCCESS;
 }
+
 
